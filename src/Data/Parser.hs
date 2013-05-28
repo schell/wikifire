0198beea9ae1f6a78ab895d0964aae55b90afa42
Schell Scivally <efsubenovex@gmail.com>
2013-05-27 18:05:45 -0700

    another parser installment

--- src/Data/Parser.hs
+++ src/Data/Parser.hs
@@ -2,12 +2,14 @@
 module Parser where
 
 import Control.Applicative  ( (<*>), (*>), (<*), (<$>), (<|>), pure )
-import Data.Text            ( Text, pack )
+import Data.Text            ( Text, pack, unpack )
+import System.FilePath      ( (</>) )
+import Data.Char            ( isSpace )
 
 import Data.Attoparsec.Text
 import Data.Attoparsec.Combinator
 
-import qualified Data.Map                   as M
+import qualified Data.Map as M
 
 {- Types -}
 
@@ -15,15 +17,15 @@ import qualified Data.Map                   as M
 type Template = [TemplateFragment]
 
 -- | Each fragment can be a sublist of fragments or a command.
-data TemplateFragment = Text            |
-                        TemplateCommand |
-                        OutputVariable  deriving (Show, Eq)
+data TemplateFragment = FragmentText    Text            |
+                        FragmentCommand TemplateCommand |
+                        FragmentOutput  OutputVariable  deriving (Show, Eq)
 
 -- | A command with a name, variable number of arguments and a map of input variables.
-data TemplateCommand = CommandName [Text] InputVariables deriving (Show, Eq)
+data TemplateCommand = RenderTemplateCommand RenderTemplateArgs deriving (Show, Eq)
 
--- | This is a list of every command name. There should not be much here :\.
-data CommandName = RenderTemplateCommand deriving (Show, Eq)
+-- | The render template command arguments.
+type RenderTemplateArgs = (FilePath, InputVariables)
 
 -- | A map for all a template's variables used to input to a render.
 type InputVariables = M.Map Text Text
@@ -38,12 +40,40 @@ type OutputVariable = Text
 
 {- Parsers -}
 
+fragmentText :: Parser TemplateFragment
+fragmentText = FragmentText . pack <$> manyTill anyChar fragmentStart
+    <?> "FragmentText"
+    where fragmentStart = try (string "[{") <|> try (string "[|")
+
+renderTemplateCommand :: Parser TemplateCommand
+renderTemplateCommand = RenderTemplateCommand <$> "renderTemplate " .*> renderTemplateArgs
+    <?> "RenderTemplateCommand"
+
+renderTemplateArgs :: Parser RenderTemplateArgs
+renderTemplateArgs = (,) <$> filepath <*> inputVariables
+
+filepath :: Parser FilePath
+filepath = unpack <$> takeTill isSpace
+    <?> "FilePath"
+
+inputVariables :: Parser InputVariables
+inputVariables = foldVariables <$> manyTill (skipSpace *> inputVariable <* skipSpace) end
+    <?> "InputVariables"
+    where foldVariables = foldl (\m (InputVariable n v) -> M.insert n v m) M.empty
+          end           = try (string "}]")
+
 outputVariable :: Parser OutputVariable
 outputVariable = string "[|" *> takeWhile1 (/= '|') <* string "|]"
-                 <?> "Parser OutputVariable"
+                 <?> "OutputVariable"
+
+inputVariable :: Parser InputVariable
+inputVariable = InputVariable <$> inputVariableName <* skipSpace <*> variableValue
+    <?> "InputVariable"
 
 inputVariableName :: Parser InputVariableName
 inputVariableName = takeWhile1 (not . isHorizontalSpace)
+    <?> "InputVariableName"
 
 variableValue :: Parser VariableValue
 variableValue = pack <$> (string "[=" *> manyTill anyChar (try (string "=]")))
+    <?> "VariableValue"
